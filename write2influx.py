41e2c370683f519a7bc1d75c9ef76a9549b3ba34
fishbeef <fishbeef@gmail.com>
2019-05-02 23:00:04 +0200

    write to influx

--- write2influx.py
+++ write2influx.py
@@ -5,7 +5,7 @@ import argparse
 
 from influxdb import InfluxDBClient
 
-def main(host='localhost', port=9999):
+def main(host='192.168.1.200', port=8086):
     """Instantiate a connection to the InfluxDB."""
     user = 'boike'
     password = '!Joerne12!'
@@ -22,7 +22,7 @@ def main(host='localhost', port=9999):
                 "host": "server01",
                 "region": "us-west"
             },
-            "time": "2009-11-10T23:00:00Z",
+            "time": "2019-05-02T23:00:00Z",
             "fields": {
                 "Float_value": 0.64,
                 "Int_value": 3,
@@ -37,14 +37,27 @@ def main(host='localhost', port=9999):
     print("Create database: " + dbname)
     client.create_database(dbname)
 
+    print("Create a retention policy")
+    client.create_retention_policy('awesome_policy', '3d', 3, default=True)
+
+    print("Write points: {0}".format(json_body))
+    client.write_points(json_body)
+
+    print("Querying data: " + query)
+    result = client.query(query)
+
+    print("Result: {0}".format(result))
+
+    
+
 def parse_args():
     """Parse the args."""
     parser = argparse.ArgumentParser(
         description='example code to play with InfluxDB')
     parser.add_argument('--host', type=str, required=False,
-                        default='localhost',
+                        default='192.168.1.200',
                         help='hostname of InfluxDB http API')
-    parser.add_argument('--port', type=int, required=False, default=9999,
+    parser.add_argument('--port', type=int, required=False, default=8086,
                         help='port of InfluxDB http API')
     return parser.parse_args()
 
